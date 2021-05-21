5266ceae6a94baab219a3af1d420fd3582a82f10
MrPig91 <sirius.cleveland@gmail.com>
2021-05-20 21:42:05 -0500

    Added Test-ChiaParameters

---
+++ PSChiaPlotter/Private/Test-ChiaParameters.ps1
@@ -0,0 +1,33 @@
+function Test-ChiaParameters {
+    param(
+        $NewJob
+    )
+    $ChiaParameters = $NewJob.InitialChiaParameters
+
+    if ($ChiaParameters.RAM -lt 3390){
+        return "RAM needs to be greater than 3390"
+    }
+    if ($ChiaParameters.Threads -le 0){
+        return "Threads needs to 1 or higher"
+    }
+    if ($NewJob.TempVolumes.Count -lt 1){
+        return "No Temp drives have been added!"
+    }
+    foreach ($tempvol in $NewJob.TempVolumes){
+        if (-not[System.IO.Directory]::Exists($tempvol.DirectoryPath)){
+            return "Temp Directory `"$($tempvol.DirectoryPath)`" does not exists"
+        }
+    }
+    if ($NewJob.FinalVolumes.Count -lt 1){
+        return "No Final Drives have been added!"
+    }
+    foreach ($finalvol in $NewJob.FinalVolumes){
+        if (-not[System.IO.Directory]::Exists($finalvol.DirectoryPath)){
+            return "Final Directory `"($($finalvol.DirectoryPath)`" does not exists"
+        }
+    }
+    if (-not[System.IO.Directory]::Exists($ChiaParameters.LogDirectory)){
+        return "Log Directory does not exists"
+    }
+    return $true
+}
