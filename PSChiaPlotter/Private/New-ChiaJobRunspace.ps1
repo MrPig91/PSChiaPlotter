0066f486ae5c3eb1a56df1d2b767c06c97b6f292
MrPig91 <sirius.cleveland@gmail.com>
2021-05-20 21:40:41 -0500

    Added New-ChiaJobRunspace Function

---
+++ PSChiaPlotter/Private/New-ChiaJobRunspace.ps1
@@ -0,0 +1,50 @@
+function New-ChiaJobRunspace{
+    param(
+        [Parameter(Mandatory)]
+        $Job
+    )
+    [powershell]::Create().AddScript{
+        Param (
+            $Job
+        )
+        $ErrorActionPreference = "Stop"
+        Add-Type -AssemblyName PresentationFramework
+        Add-Type -AssemblyName System.Windows.Forms
+
+        #Import required assemblies and private functions
+        Get-childItem -Path $DataHash.PrivateFunctions -File | ForEach-Object {Import-Module $_.FullName}
+        Get-childItem -Path $DataHash.Classes -File | ForEach-Object {Import-Module $_.FullName}
+
+        for ($queue = 1; $queue -le $Job.QueueCount; $queue++){
+            try{
+                $newQueue = [PSChiaPlotter.ChiaQueue]::new($Job.JobNumber,$queue,$job.InitialChiaParameters)
+                $newQueue.Status = "Waiting"
+                $DataHash.MainViewModel.AllQueues.Add($newQueue)
+                $Job.Queues.Add($newQueue)
+            }
+            catch{
+                Show-Messagebox -Text $_.Exception.Message -Title "Job $($Job.JobNumber) - Runspace"
+            }
+        }
+
+        try{
+            for ($queue = 0;$queue -lt $Job.QueueCount;$queue++){
+                if ($queue -eq 0){
+                    sleep -Seconds $Job.FirstDelay
+                    $Job.Status = "Running"
+                }
+                #$Job.Queues[$queue].StartTime = [DateTime]::Now
+                $Job.Queues[$queue].Status = "Running"
+                $QueueRunspace = New-ChiaQueueRunspace -Queue $Job.Queues[$queue] -Job $Job
+                $QueueRunspace.Runspacepool = $ScriptsHash.Runspacepool
+                $QueueRunspace.BeginInvoke()
+                if (($queue + 1) -ne $Job.QueueCount){
+                    sleep $job.DelayInSeconds
+                }
+            }
+        }
+        catch{
+            Show-Messagebox -Text $_.Exception.Message -Title "Job $($Job.JobNumber) - Runspace"
+        }
+    }.AddParameters($PSBoundParameters)
+}
