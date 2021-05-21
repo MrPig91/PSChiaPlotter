618e27ce1fcc3ac0ddc88997ca51faa61cab2bbd
MrPig91 <sirius.cleveland@gmail.com>
2021-05-20 22:43:10 -0500

    Matched Volume with disk and partitions

--- PSChiaPlotter/Private/Get-ChiaVolume.ps1
+++ PSChiaPlotter/Private/Get-ChiaVolume.ps1
@@ -2,15 +2,33 @@ function Get-ChiaVolume {
     [CmdletBinding()]
     param()
 
+    #grabbing all volumes, partitions, disks, and physicaldisks at once since it has proven to be faster
+    $AllVolumes = Get-CimInstance -Namespace "ROOT/Microsoft/Windows/Storage" -ClassName MSFT_Volume |
+        Where {$_.DriveLetter -ne $Null} | sort -Property DriveLetter
+    $AllPartitions = Get-Partition
+    $AllDisks = Get-Disk
+    $AllphysicalDisk = Get-PhysicalDisk
 
-    $Volumes = Get-CimInstance -Namespace "ROOT/Microsoft/Windows/Storage" -ClassName MSFT_Volume |
-        Where {$_.DriveLetter -ne $Null}
+    foreach ($volume in $AllVolumes){
+        try{
+            $partition = $AllPartitions | where DriveLetter -eq $volume.DriveLetter
+            $disk = $AllDisks | where DiskNumber -eq $partition.DiskNumber
+            $physicalDisk = $AllphysicalDisk | where UniqueId -eq $disk.UniqueId
 
-    foreach ($volume in $Volumes){
-        $Label = $volume.FileSystemLabel
-        if ([string]::IsNullOrEmpty($volume.FileSystemLabell)){
-            $Label = "N/A"
+            $Label = $volume.FileSystemLabel
+            if ([string]::IsNullOrEmpty($volume.FileSystemLabell)){
+                $Label = "N/A"
+            }
+            if ($physicalDisk){
+                $ChiaVolume = [PSChiaPlotter.ChiaVolume]::new($volume.DriveLetter,$Label,$volume.Size,$volume.SizeRemaining)
+                $ChiaVolume.BusType = $physicalDisk.BusType
+                $ChiaVolume.MediaType = $physicalDisk.MediaType
+                $ChiaVolume
+                Clear-Variable PhysicalDisk,Disk,Partition
+            }
         }
-        [PSChiaPlotter.ChiaVolume]::new($volume.DriveLetter,$Label,$volume.Size,$volume.SizeRemaining)
-    }
+        catch{
+            Write-Warning "Unable to create a ChiaVolume from driveletter $($volume.DriveLetter)"
+        }
+    } #volume
 }
