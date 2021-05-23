6a4fe7a1a0a4b2d5a3d9f8eac9dff2f80fd4711e
MrPig91 <sirius.cleveland@gmail.com>
2021-05-22 22:57:27 -0500

    fixed var name and change sizeremainging to freespace

--- PSChiaPlotter/Private/Get-BestChiaFinalDrive.ps1
+++ PSChiaPlotter/Private/Get-BestChiaFinalDrive.ps1
@@ -5,13 +5,13 @@ function Get-BestChiaFinalDrive {
     )
     $requiredFinalSize = 101.4 * 1gb
 
-    foreach ($finalpvol in $ChiaVolumes){
+    foreach ($finalvol in $ChiaVolumes){
         $newVolumeInfo = Get-Volume -DriveLetter $finalvol.DriveLetter
         $finalvol.FreeSpace = $newVolumeInfo.SizeRemaining
     }
     $BestVolume = $ChiaVolumes | sort -Property FreeSpace -Descending | Select -First 1
     $MasterVolume = $DataHash.MainViewModel.AllVolumes | where DriveLetter -eq $BestVolume.DriveLetter
-    if (($finalvol.SizeRemaining - ($MasterVolume.PendingPlots * 101.425)) -gt $requiredFinalSize){
+    if (($finalvol.FreeSpace - ($MasterVolume.PendingPlots * 101.425)) -gt $requiredFinalSize){
             $BestVolume
     }
 }
