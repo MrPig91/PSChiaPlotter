38b83e284415b4153379c480fab40e1a6d355002
MrPig91 <sirius.cleveland@gmail.com>
2021-05-28 16:45:45 -0500

    Imporved BestFinaldrive and temp funtion

--- PSChiaPlotter/Private/Get-BestChiaFinalDrive.ps1
+++ PSChiaPlotter/Private/Get-BestChiaFinalDrive.ps1
@@ -3,15 +3,17 @@ function Get-BestChiaFinalDrive {
     param(
         $ChiaVolumes
     )
-    $requiredFinalSize = 101.4 * 1gb
+    $finalplotsize = 101.4 * 1gb
 
     foreach ($finalvol in $ChiaVolumes){
         $newVolumeInfo = Get-Volume -DriveLetter $finalvol.DriveLetter
         $finalvol.FreeSpace = $newVolumeInfo.SizeRemaining
     }
-    $BestVolume = $ChiaVolumes | sort -Property FreeSpace -Descending | Select -First 1
-    $MasterVolume = $DataHash.MainViewModel.AllVolumes | where DriveLetter -eq $BestVolume.DriveLetter
-    if (($finalvol.FreeSpace - ($MasterVolume.PendingFinalRuns.Count * 101.425)) -gt $requiredFinalSize){
-            $BestVolume
+    $sortedVolumes = $ChiaVolumes | sort -Property FreeSpace -Descending
+    foreach ($volume in $sortedVolumes){
+        $MasterVolume = $DataHash.MainViewModel.AllVolumes | where DriveLetter -eq $volume.DriveLetter
+        if (($volume.FreeSpace - ($MasterVolume.PendingFinalRuns.Count * $finalplotsize)) -gt $finalplotsize){
+                return $volume
+        }
     }
 }
--- PSChiaPlotter/Private/Get-BestChiaTempDrive.ps1
+++ PSChiaPlotter/Private/Get-BestChiaTempDrive.ps1
@@ -4,16 +4,19 @@ function Get-BestChiaTempDrive {
         $ChiaVolumes
     )
 
-    $requiredTempSize = (239 * 1gb)
+    $requiredTempSize = 239 * 1gb
+    $finalplotsize = 101.4 * 1gb
     foreach ($tempvol in $ChiaVolumes){
         $newVolumeInfo = Get-Volume -DriveLetter $tempvol.DriveLetter
         $tempvol.FreeSpace = $newVolumeInfo.SizeRemaining
     }
-    $BestVolume = $ChiaVolumes | sort -Property FreeSpace -Descending | Select -First 1
-    $MasterVolume = $DataHash.MainViewModel.AllVolumes | where DriveLetter -eq $BestVolume.DriveLetter
-    if ($MasterVolume.CurrentChiaRuns.Count -lt $MasterVolume.MaxConCurrentTempChiaRuns){
-        if (($tempvol.FreeSpace - ($MasterVolume.PendingFinalRuns.Count * 101.4)) -gt $requiredTempSize){
-            $BestVolume
+    $sortedVolumes = $ChiaVolumes | sort -Property FreeSpace -Descending
+    foreach ($volume in $sortedVolumes){
+        $MasterVolume = $DataHash.MainViewModel.AllVolumes | where DriveLetter -eq $volume.DriveLetter
+        if ($MasterVolume.CurrentChiaRuns.Count -lt $volume.MaxConCurrentTempChiaRuns){
+            if (($volume.FreeSpace - ($MasterVolume.PendingFinalRuns.Count * $finalplotsize)) -gt $requiredTempSize){
+                return $volume
+            }
         }
     }
 }
