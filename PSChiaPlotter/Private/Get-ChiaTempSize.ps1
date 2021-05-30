1006440f7adc7d9f9105c8e119e99273b4fc1326
MrPig91 <sirius.cleveland@gmail.com>
2021-05-30 00:12:49 -0500

    Added Try Catch and return 0 if fails

--- PSChiaPlotter/Private/Get-ChiaTempSize.ps1
+++ PSChiaPlotter/Private/Get-ChiaTempSize.ps1
@@ -4,11 +4,16 @@ function Get-ChiaTempSize{
         $DirectoryPath,
         $PlotId
     )
-    if ($PlotId -ne $null){
-        $tepmSize = (Get-ChildItem -Path $DirectoryPath -Filter "*$plotid*.tmp" | Measure-Object -Property Length -Sum).Sum
-        return [math]::Round($tepmSize)
+    try{
+        if ($PlotId -ne $null){
+            $tepmSize = (Get-ChildItem -Path $DirectoryPath -Filter "*$plotid*.tmp" | Measure-Object -Property Length -Sum).Sum
+            return [math]::Round($tepmSize / 1gb)
+        }
+        else{
+            return 0
+        }
     }
-    else{
+    catch{
         return 0
     }
 }
