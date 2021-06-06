77438831bed1669bed923014d5d59a98e0ff70ce
MrPig91 <sirius.cleveland@gmail.com>
2021-06-05 20:40:54 -0500

    New Function Update-ChiaVolume

---
+++ PSChiaPlotter/Private/Update-ChiaVolume.ps1
@@ -0,0 +1,24 @@
+function Update-ChiaVolume {
+    [CmdletBinding()]
+    param()
+    $Volumes = Get-ChiaVolume
+    $CurrentVolumes = $Volumes | where DriveLetter -in $DataHash.MainViewModel.AllVolumes.DriveLetter
+    foreach ($volume in $CurrentVolumes){
+        $matchedVolume = $DataHash.MainViewModel.AllVolumes | where DriveLetter -eq $volume.DriveLetter
+        if ($matchedVolume){
+            $matchedVolume.FreeSpace = $volume.FreeSpace
+            $matchedVolume = $null
+        }
+        Write-Host "Drive $($volume.DriveLetter) refreshed"
+    }
+
+    $newVolumes = $Volumes | where DriveLetter -notin $DataHash.MainViewModel.AllVolumes.DriveLetter
+    foreach ($newvolume in $newVolumes){
+        $DataHash.MainViewModel.AllVolumes.Add($newvolume)
+    }
+
+    $removedVolumes = $DataHash.MainViewModel.AllVolumes | where DriveLetter -NotIn $Volumes.DriveLetter
+    foreach ($removedvolume in $removedVolumes){
+        $DataHash.MainViewModel.AllVolumes.Remove($removedvolume)
+    }
+}
