8617843f146a03822f59414cc570d689afe5ab04
sk00t3r <johnsone@roadrunner.com>
2021-06-06 16:14:37 -0400

    Update Get-ChiaVolume.ps1

--- PSChiaPlotter/Private/Get-ChiaVolume.ps1
+++ PSChiaPlotter/Private/Get-ChiaVolume.ps1
@@ -2,11 +2,15 @@ function Get-ChiaVolume {
     [CmdletBinding()]
     param()
 
-    #grabbing all volumes, partitions, disks, and physicaldisks at once since it has proven to be faster
-    $AllVolumes = Get-CimInstance -Namespace "ROOT/Microsoft/Windows/Storage" -ClassName MSFT_Volume |
-        Where {$_.DriveLetter -ne $Null} | sort -Property DriveLetter
-    $AllPartitions = Get-Partition
+$LocalVolumes = Get-CimInstance -Namespace "ROOT/Microsoft/Windows/Storage" -ClassName MSFT_Volume | Where {$_.DriveLetter -ne $Null} | sort -Property DriveLetter
+$RemoteVolumes = Get-PSDrive | where {$_.DisplayRoot -match "\\"}
+
+#grabbing all volumes, partitions, disks, physicaldisks, and mapped drives at once since it has proven to be faster
+
+$AllVolumes = $LocalVolumes + $RemoteVolumes
+$AllPartitions = Get-Partition
     $AllDisks = Get-Disk
+    $AllMappedDrives = $RemoteVolumes
     $AllphysicalDisk = Get-PhysicalDisk
 
     foreach ($volume in $AllVolumes){
@@ -14,6 +18,7 @@ function Get-ChiaVolume {
             $partition = $AllPartitions | where DriveLetter -eq $volume.DriveLetter
             $disk = $AllDisks | where DiskNumber -eq $partition.DiskNumber
             $physicalDisk = $AllphysicalDisk | where DeviceId -eq $disk.DiskNumber
+            $mappedDrive = $AllMappedDrives | where DriveLetter -eq $volume.DriveLetter
 
             $Label = $volume.FileSystemLabel
             if ([string]::IsNullOrEmpty($volume.FileSystemLabel)){
@@ -25,13 +30,25 @@ function Get-ChiaVolume {
                 $ChiaVolume.MediaType = $physicalDisk.MediaType
                 $MaxTempCount = [math]::Floor([decimal]($volume.size / (239 * 1gb)))
                 $ChiaVolume.MaxConCurrentTempChiaRuns = $MaxTempCount
-                $ChiaVolume
                 Clear-Variable PhysicalDisk,Disk,Partition,MaxTempCount
+            }
+               elseif ($mappedDrive){
+                $ChiaVolume = [PSChiaPlotter.ChiaVolume]::new($volume.DriveLetter,$Label,$volume.Size,$volume.SizeRemaining)
+                $ChiaVolume.BusType = $physicalDisk.BusType
+                $ChiaVolume.MediaType = $physicalDisk.MediaType
+                $MaxTempCount = [math]::Floor([decimal]($volume.size / (239 * 1gb)))
+                $ChiaVolume.MaxConCurrentTempChiaRuns = $MaxTempCount
+                $ChiaVolume
+                Clear-Variable mappedDrive,Disk,Partition,MaxTempCount
             }
         }
         catch{
             Write-PSChiaPlotterLog -LogType "Error" -LineNumber $_.InvocationInfo.ScriptLineNumber -Message $_.Exception.Message -DebugLogPath $DataHash.LogPath
-            Write-Warning "Unable to create a ChiaVolume from driveletter $($volume.DriveLetter)"
-        }
-    } #volume
+            $DriveLetter = $volume.DriveLetter
+            if ([string]::IsNullOrEmpty($volume.DriveLetter)){
+            $DriveLetter = $volume.Name
+            }
+        Write-Warning "Unable to create a ChiaVolume from driveletter $($DriveLetter)"
+      }
+   }
 }
