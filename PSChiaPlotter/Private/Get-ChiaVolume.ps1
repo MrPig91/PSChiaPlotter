6cb2e0a5ba10a60b1f2fd869f9ba0f4ced4f38dd
MrPig91 <sirius.cleveland@gmail.com>
2021-06-07 20:16:08 -0500

    Revamped Get-ChiaVolume to get all volumes without drive letters or physical disk

--- PSChiaPlotter/Private/Get-ChiaVolume.ps1
+++ PSChiaPlotter/Private/Get-ChiaVolume.ps1
@@ -3,35 +3,49 @@ function Get-ChiaVolume {
     param()
 
     #grabbing all volumes, partitions, disks, and physicaldisks at once since it has proven to be faster
-    $AllVolumes = Get-CimInstance -Namespace "ROOT/Microsoft/Windows/Storage" -ClassName MSFT_Volume |
-        Where {$_.DriveLetter -ne $Null} | sort -Property DriveLetter
-    $AllPartitions = Get-Partition
+    $AllVolumes = Get-Volume
+    #filter out all paritions not are not accessible to the file system
+    $AllPartitions = Get-Partition | Where {$_.AccessPaths.Count -gt 1}
     $AllDisks = Get-Disk
     $AllphysicalDisk = Get-PhysicalDisk
 
     foreach ($volume in $AllVolumes){
         try{
-            $partition = $AllPartitions | where DriveLetter -eq $volume.DriveLetter
+            $partition = $AllPartitions | where AccessPaths -Contains "$($volume.UniqueId)"
             $disk = $AllDisks | where DiskNumber -eq $partition.DiskNumber
             $physicalDisk = $AllphysicalDisk | where DeviceId -eq $disk.DiskNumber
+            if ($physicalDisk -ne $null){
+                $MediaType = $physicalDisk.MediaType
+            }
+            else{
+                $MediaType = "Unknown"
+            }
 
             $Label = $volume.FileSystemLabel
             if ([string]::IsNullOrEmpty($volume.FileSystemLabel)){
                 $Label = "N/A"
             }
-            if ($physicalDisk){
-                $ChiaVolume = [PSChiaPlotter.ChiaVolume]::new($volume.DriveLetter,$Label,$volume.Size,$volume.SizeRemaining)
+            $DriveLetter = $volume.DriveLetter
+            if (-not[char]::IsLetter($DriveLetter)){
+                $DriveLetter = '?'
+            }
+            if ($partition){
+                $DirectoryPaths = $partition.AccessPaths | where {$_ -ne $volume.UniqueId}
+                $ChiaVolume = [PSChiaPlotter.ChiaVolume]::new($volume.UniqueId,$Label,$volume.Size,$volume.SizeRemaining)
                 $ChiaVolume.BusType = $physicalDisk.BusType
-                $ChiaVolume.MediaType = $physicalDisk.MediaType
+                $ChiaVolume.MediaType = $MediaType
                 $MaxTempCount = [math]::Floor([decimal]($volume.size / (239 * 1gb)))
                 $ChiaVolume.MaxConCurrentTempChiaRuns = $MaxTempCount
+                $ChiaVolume.DriveLetter = $DriveLetter
+                $ChiaVolume.DirectoryPath = $DirectoryPaths | select -First 1
+                $DirectoryPaths | foreach {$ChiaVolume.AccessPaths.Add($_)}
                 $ChiaVolume
-                Clear-Variable PhysicalDisk,Disk,Partition,MaxTempCount
+                Clear-Variable PhysicalDisk,Disk,Partition,MaxTempCount -ErrorAction SilentlyContinue
             }
         }
         catch{
             Write-PSChiaPlotterLog -LogType "Error" -LineNumber $_.InvocationInfo.ScriptLineNumber -Message $_.Exception.Message -DebugLogPath $DataHash.LogPath
-            Write-Warning "Unable to create a ChiaVolume from driveletter $($volume.DriveLetter)"
+            #Write-Warning "Unable to create a ChiaVolume from driveletter $($volume.DriveLetter)"
         }
     } #volume
 
@@ -48,18 +62,25 @@ function Get-ChiaVolume {
             }
             if (-not[string]::IsNullOrEmpty($drive.DeviceID)){
                 $DriveLetter = $drive.DeviceID.TrimEnd(':')
-                $ChiaVolume = [PSChiaPlotter.ChiaVolume]::new($DriveLetter,$Label,$drive.Size,$drive.FreeSpace)
+                $ChiaVolume = [PSChiaPlotter.ChiaVolume]::new($drive.VolumeSerialNumber,$Label,$drive.Size,$drive.FreeSpace)
                 $ChiaVolume.BusType = $BusType
                 $ChiaVolume.MediaType = $MediaType
                 $MaxTempCount = [math]::Floor([decimal]($drive.size / (239 * 1gb)))
                 $ChiaVolume.MaxConCurrentTempChiaRuns = $MaxTempCount
+                $ChiaVolume.DriveLetter = $DriveLetter
+                $DirectoryPath = $DriveLetter + ':\'
+                $ChiaVolume.DirectoryPath = $DirectoryPath
+                $ChiaVolume.AccessPaths.Add($DirectoryPath)
+                if (Test-Path $label){
+                    $ChiaVolume.AccessPaths.Add($Label)
+                }
                 $ChiaVolume
                 Clear-Variable DriveLetter
             }
         }
         catch{
             Write-PSChiaPlotterLog -LogType "Error" -LineNumber $_.InvocationInfo.ScriptLineNumber -Message $_.Exception.Message -DebugLogPath $DataHash.LogPath
-            Write-Warning "Unable to create a ChiaVolume from driveletter $($DriveLetter.DriveLetter)"
+            #Write-Warning "Unable to create a ChiaVolume from driveletter $($DriveLetter.DriveLetter)"
         }
     }
 }
