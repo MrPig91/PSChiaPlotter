e061c7b2c881262fb537b2fc5920e2067dc2373b
MrPig91 <sirius.cleveland@gmail.com>
2021-06-08 23:42:22 -0500

    Changed DriveLetter To UniqueId

--- PSChiaPlotter/Private/Update-ChiaVolume.ps1
+++ PSChiaPlotter/Private/Update-ChiaVolume.ps1
@@ -2,21 +2,21 @@ function Update-ChiaVolume {
     [CmdletBinding()]
     param()
     $Volumes = Get-ChiaVolume
-    $CurrentVolumes = $Volumes | where DriveLetter -in $DataHash.MainViewModel.AllVolumes.DriveLetter
+    $CurrentVolumes = $Volumes | where UniqueId -in $DataHash.MainViewModel.AllVolumes.UniqueId
     foreach ($volume in $CurrentVolumes){
-        $matchedVolume = $DataHash.MainViewModel.AllVolumes | where DriveLetter -eq $volume.DriveLetter
+        $matchedVolume = $DataHash.MainViewModel.AllVolumes | where UniqueId -eq $volume.UniqueId
         if ($matchedVolume){
             $matchedVolume.FreeSpace = $volume.FreeSpace
             $matchedVolume = $null
         }
     }
 
-    $newVolumes = $Volumes | where DriveLetter -notin $DataHash.MainViewModel.AllVolumes.DriveLetter
+    $newVolumes = $Volumes | where UniqueId -notin $DataHash.MainViewModel.AllVolumes.UniqueId
     foreach ($newvolume in $newVolumes){
         $DataHash.MainViewModel.AllVolumes.Add($newvolume)
     }
 
-    $removedVolumes = $DataHash.MainViewModel.AllVolumes | where DriveLetter -NotIn $Volumes.DriveLetter
+    $removedVolumes = $DataHash.MainViewModel.AllVolumes | where UniqueId -NotIn $Volumes.UniqueId
     foreach ($removedvolume in $removedVolumes){
         $DataHash.MainViewModel.AllVolumes.Remove($removedvolume)
     }
