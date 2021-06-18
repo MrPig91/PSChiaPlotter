95a2871349a5ea138324c2c3f32dc753df16afa6
MrPig91 <sirius.cleveland@gmail.com>
2021-06-17 22:33:26 -0500

    Finished up Invoke-AddOldPlotDirecotyButtonClick

--- PSChiaPlotter/Private/ButtonClicks/Invoke-AddOldPlotDirectoryButtonClick.ps1
+++ PSChiaPlotter/Private/ButtonClicks/Invoke-AddOldPlotDirectoryButtonClick.ps1
@@ -5,25 +5,33 @@ function Invoke-AddOldPlotDirectoryButtonClick {
     )
 
     try{
+        if (-not$Path.StartsWith($OldDirectories_ListBox.DataContext.DriveLetter)){
+            Show-MessageBox "The replot directory path does not exists in the volume you are trying to replot!" | Out-Null
+            return
+        }
+        if ($OldDirectories_ListBox.DataContext.DirectoryPath -eq $path){
+            Show-MessageBox -Text "Your 'New Plot Directory' cannot be the same as a replot diretory!`n`nPlease change your 'New Plot Directory'!" -Icon Warning | Out-Null
+            return
+        }
         if ([System.IO.Directory]::Exists($Path)){
-            
-            $currentKSize = $NewJobViewModel.NewChiaJob.InitialChiaParameters.KSize.KSizeValue
+
+            if ($Path -in $OldDirectories_ListBox.DataContext.OldPlotDirectories.Path){
+                Show-MessageBox -Icon Warning -Text "This old plot directory has already been added!" | Out-Null
+                return
+            }
+            $currentKSize = $DataHash.NewJobViewModel.NewChiaJob.InitialChiaParameters.KSize.KSizeValue
             $plots = Get-ChildItem -Path $Path -Filter "*.plot"
             $matchedPlots = $plots | where Name -like "plot-k$($currentKSize)*.plot"
             $plotcount = ($matchedPlots | Measure-Object).Count
 
             if ($plotcount -ge 1){
-                return [PSCustomObject]@{
-                    Path = $Path
-                    PlotCount = $plotcount
-                }
-                
+                [PSChiaPlotter.OldPlotDirectory]::new($path,$plotcount,$currentKSize)
             }
             elseif (($plots | Measure-Object).Count -ge 1){
-                Show-MessageBox -Text "Plots were found, but they are not K$currentKSize and cannot be plotted with the current settings!" -Icon Warning | Out-Null
+                Show-MessageBox -Text "Plots were found, but they are not K$currentKSize and cannot be replotted with the current settings!" -Icon Warning | Out-Null
             }
             else{
-                Show-MessageBox -Text "No plots were found of any KSize for this directory!" -Icon Warning | Out-Null
+                Show-MessageBox -Text "No plots were found of any KSize in this directory!" -Icon Warning | Out-Null
             }
         }
         else{
