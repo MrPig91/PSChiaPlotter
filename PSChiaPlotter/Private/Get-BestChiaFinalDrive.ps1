2d4cb19b87904c385f101919278b67eab37455f8
MrPig91 <sirius.cleveland@gmail.com>
2021-06-18 00:28:09 -0500

    Added Logic to grab the best final drive while plotting (need to review)

--- PSChiaPlotter/Private/Get-BestChiaFinalDrive.ps1
+++ PSChiaPlotter/Private/Get-BestChiaFinalDrive.ps1
@@ -14,10 +14,29 @@ function Get-BestChiaFinalDrive {
         $MasterVolume = $DataHash.MainViewModel.AllVolumes | where UniqueId -eq $finalvol.UniqueId
         $finalvol.PendingFinalRuns = $MasterVolume.PendingFinalRuns
     }
-    $sortedVolumes = $ChiaVolumes | Sort-Object -Property @{Expression = {$_.PendingFinalRuns.Count}; Descending = $false},@{Expression = "FreeSpace"; Descending = $True}
-    foreach ($volume in $sortedVolumes){
-        if (($volume.FreeSpace - ($Volume.PendingFinalRuns.Count * $finalplotsize)) -gt $finalplotsize){
-                return $volume
+    if ($ChiaJob.ReplotEnabled){
+        foreach ($replotVolume in $ChiaVolumes){
+            foreach ($oldDirectory in $replotVolume.OldPlotDirectories){
+                try{
+                    $oldplotcount = (Get-ChildItem -Path $oldDirectory.Path -Filter "plot-k$($oldDirectory.KSizeValue)*.plot" | Measure-Object).Count
+                    $oldDirectory.PlotCount = $oldplotcount
+                }
+                catch{
+                    $oldDirectory.PlotCount = 0
+                    Write-PSChiaPlotterLog -LogType "ERROR" -ErrorObject $_
+                }
+            }
+            $replotVolume.TotalReplotCount = ($replotVolume.OldPlotDirectories | Measure-Object -Property PlotCount -Sum).Sum
+        }
+        $AvailableVolumes = $ChiaVolumes | where TotalReplotCount -ne 0 | sort -Property @{Expression = {$_.PendingFinalRuns.Count}; Descending = $false},@{Expression = "TotalReplotCount"; Descending = $True}
+        return ($AvailableVolumes | select -First 1)
+    }
+    else{
+        $sortedVolumes = $ChiaVolumes | Sort-Object -Property @{Expression = {$_.PendingFinalRuns.Count}; Descending = $false},@{Expression = "FreeSpace"; Descending = $True}
+        foreach ($volume in $sortedVolumes){
+            if (($volume.FreeSpace - ($Volume.PendingFinalRuns.Count * $finalplotsize)) -gt $finalplotsize){
+                    return $volume
+            }
         }
     }
 }
